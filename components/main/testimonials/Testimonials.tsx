cb841a1e0a1a05c10bee5f96fd438079914bb734
moyasiginko <mrsiyam61@gmail.com>
2025-05-09 02:13:12 +0600

    add icons to the clients

--- components/main/testimonials/Testimonials.tsx
+++ components/main/testimonials/Testimonials.tsx
@@ -193,20 +193,12 @@ const Testimonials = () => {
                   }}
                   className="flex flex-col justify-center h-full"
                 >
-                  <svg
-                    width="40"
-                    height="40"
-                    viewBox="0 0 40 40"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="text-pink-500"
-                  >
-                    <path
-                      d="M12 28H4C2.89543 28 2 27.1046 2 26V20C2 13.3726 7.37258 8 14 8H16C17.1046 8 18 8.89543 18 10V12C18 13.1046 17.1046 14 16 14H14C10.6863 14 8 16.6863 8 20V24C8 24.5523 8.44772 25 9 25H12C13.1046 25 14 25.8954 14 27V28C14 29.1046 13.1046 30 12 30V28ZM32 28H24C22.8954 28 22 27.1046 22 26V20C22 13.3726 27.3726 8 34 8H36C37.1046 8 38 8.89543 38 10V12C38 13.1046 37.1046 14 36 14H34C30.6863 14 28 16.6863 28 20V24C28 24.5523 28.4477 25 29 25H32C33.1046 25 34 25.8954 34 27V28C34 29.1046 33.1046 30 32 30V28Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <p className="text-[28px] font-medium text-gray-200 mb-6 text-left">
+                  <img
+                    src="/images/logo/quote.svg"
+                    alt="Quote"
+                    className="mb-4 w-15 h-15"
+                  />
+                  <p className="text-[28px] leading-tight font-medium text-gray-200 mb-6 text-left">
                     I Wanted Someone To Help Me With My YouTube Channel, The
                     Seller Did A Great Job And I'm Very Happy With The Result,
                   </p>
@@ -220,21 +212,15 @@ const Testimonials = () => {
                     </span>
                   </div>
                 </div>
-
-                <div className="absolute -right-7 items-center bg-black p-2 rounded-full border border-gray-800">
-                  <div className="bg-pink-500 rounded-full p-2">
-                    <svg
-                      width="20"
-                      height="20"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M21 8.25L14.5 12L21 15.75V8.25Z" fill="white" />
-                      <path d="M10 8.25L3.5 12L10 15.75V8.25Z" fill="white" />
-                    </svg>
+                <a href="#services">
+                  <div className="absolute w-[109px] h-[109px] -right-14 flex items-center justify-center bg-[#241E20] rounded-full shadow-[0px_0px_45px_-9px_rgba(0,0,0,0.5)]">
+                    <img
+                      src="/images/logo/gallery.svg"
+                      alt="Quote"
+                      className="w-12 h-12"
+                    />
                   </div>
-                </div>
+                </a>
               </div>
             </div>
 
