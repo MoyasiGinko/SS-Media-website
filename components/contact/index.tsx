56dbe6b9a1d9559d8043764d9c54d6834f226a77
moyasiginko <mrsiyam61@gmail.com>
2025-05-10 03:53:30 +0600

    Update index.tsx

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -5,15 +5,15 @@ import QuickChatSection from "@/components/contact/cta/QuickChatSection";
 
 export default function ContactPage() {
   return (
-    <div className="min-h-screen bg-black relative ">
+    <div className="min-h-screen bg-white relative ">
       {/* Top Section: Split Layout */}
       <div className="flex -z-0 flex-col md:flex-row min-h-[70vh]">
         {/* Left: Intro */}
-        <div className="flex-1 flex flex-col items-center justify-center">
+        <div className="flex-1 flex flex-col bg-black items-center justify-center">
           <ContactIntro />
         </div>
         {/* Right: Form */}
-        <div className="flex-1 flex items-center justify-center bg-gradient-to-br from-gray-200 via-purple-100 to-pink-200">
+        <div className="flex-1 flex items-center justify-center bg-gradient-to-br from-gray-50 via-[#BB6FFB]/50 to-[#FC5F67]/50">
           <ZoomCallForm />
         </div>
       </div>
