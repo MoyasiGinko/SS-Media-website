d65b95aa8e11e86e8caa82e08bd263070cc4914a
moyasiginko <mrsiyam61@gmail.com>
2025-05-10 02:44:20 +0600

    contact intro

--- components/contact/intro/ContactIntro.tsx
+++ components/contact/intro/ContactIntro.tsx
@@ -3,17 +3,17 @@ import React from "react";
 export const ContactIntro = () => {
   return (
     <div className="text-center">
-      <h1 className="text-4xl md:text-5xl font-bold text-white mb-4">
+      <h1 className="text-4xl md:text-7xl max-w-[691px] syne-unique font-bold text-white mb-5">
         We Are Here
         <br />
         For Your Success!
       </h1>
-      <p className="text-gray-300 mb-6">
+      <p className="text-white/70 max-w-[673px] text-[28px] mb-15">
         Fill The Contact Form Or Scan Those QR. It's An Honor To Support You In
         Your Content Creation Journey.
       </p>
-      <button className="px-6 py-2 rounded-lg bg-gradient-to-r from-purple-700 to-orange-400 text-white font-semibold shadow">
-        Quick Chat!
+      <button className="w-[160px] h-[50px] rounded-[22px] text-xl text-center border-[1.5px] border-white bg-gradient-to-r from-[#BB6FFB]/30 via-[#FC5F67]/30 to-[#FFB054]/30 text-white font-medium  items-center">
+        Quick Chat
       </button>
     </div>
   );
