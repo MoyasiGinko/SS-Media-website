717c2e02f604fb36dd2c2f1dd37782120ea9d11f
moyasiginko <mrsiyam61@gmail.com>
2025-05-10 04:37:45 +0600

    fix the links

--- components/common/HomeButton.tsx
+++ components/common/HomeButton.tsx
@@ -2,7 +2,10 @@ import React from "react";
 export const BackHomeButton = () => {
   return (
     <div className="max-w-6xl fixed top-0 z-999 flex justify-start mx-auto pl-16 pt-14">
-      <button className="w-[160px] h-[50px] rounded-[22px] text-xl text-center bg-gradient-to-r from-[#BB6FFB] via-[#FC5F67] to-[#FFB054] text-black font-medium justify-center self-start flex items-center">
+      <a
+        href="/"
+        className="w-[160px] inline-flex h-[50px] rounded-[22px] text-xl text-center bg-gradient-to-r from-[#BB6FFB] via-[#FC5F67] to-[#FFB054] text-black font-medium justify-center self-start  items-center"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5 mr-2"
@@ -18,7 +21,7 @@ export const BackHomeButton = () => {
           />
         </svg>
         Home
-      </button>
+      </a>
     </div>
   );
 };
