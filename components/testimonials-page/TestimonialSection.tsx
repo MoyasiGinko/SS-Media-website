a0b55c424692d7c80f9986fb7e99a3618dc171f4
moyasiginko <mrsiyam61@gmail.com>
2025-05-16 12:59:29 +0600

    Update TestimonialSection.tsx

--- components/testimonials-page/TestimonialSection.tsx
+++ components/testimonials-page/TestimonialSection.tsx
@@ -12,7 +12,7 @@ const TestimonialSection: React.FC = () => {
   ];
 
   return (
-    <section className="flex flex-col w-full max-w-7xl mx-auto  lg:px-16 pt-20 md:pt-40 gap-8 md:flex-row md:items-center md:justify-between">
+    <section className="flex flex-col w-full max-w-7xl mx-auto px-4 lg:px-16 pt-30 md:pt-40 gap-8 md:flex-row md:items-center justify-center lg:justify-between">
       <div className="space-y-4 md:max-w-lg lg:max-w-2xl">
         <div className="flex items-center space-x-2">
           <div className="flex -space-x-2">
@@ -27,32 +27,34 @@ const TestimonialSection: React.FC = () => {
               />
             ))}
           </div>
-          <span className="text=[20px] xl:text-[28px] text-gray-400">
+          <span className="text-sm md:text-base lg:text-lg xl:text-xl text-gray-400">
             20+ Satisfied Clients
           </span>
         </div>
 
-        <h1 className="text-2xl lg:text-4xl xl:text-[60px] syne-unique font-semibold leading-tight">
+        <h1 className="text-xl md:text-2xl lg:text-4xl xl:text-[60px] syne-unique font-semibold leading-tight">
           Testimonial From
           <br />
           Our Clients
         </h1>
 
-        <p className="text-sm text-gray-400 max-w-sm">
+        <p className="text-xs md:text-sm lg:text-base text-gray-400 max-w-sm">
           Hear What Our Clients Have To Say About The Quality Of Our Edits And
           How We Helped Bring Their Vision To Life.
         </p>
 
-        <Button variant="purple" className="text-xs px-4 py-2 w-max">
+        <Button variant="purple" className="text-xs md:text-sm px-4 py-2 w-max">
           View All
         </Button>
       </div>
 
-      <div className="flex-shrink-0 w-full max-w-[540px]">
-        <img
+      <div className="flex-shrink-0 w-full md:max-w-[440px] lg:max-w-[540px]">
+        <Image
           src="/images/testimonials/t1.png"
           alt="Smiling woman in green t-shirt sitting at desk with glasses in hand, laptop, and modern room background with plants and books"
-          className="w-full h-full  rounded-xl object-cover"
+          width={540}
+          height={360}
+          className="w-full h-auto rounded-xl object-cover"
         />
       </div>
     </section>
